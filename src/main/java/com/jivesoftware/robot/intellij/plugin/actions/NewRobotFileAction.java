faba62f2138f91014be4c33a234533a49e924ad8
Charles Capps <charles.capps@jivesoftware.com>
2014-04-11 16:37:24 -0700

    Version 1.0.6- added templates for creating robot files,
    one for a Robot Test Case File, and another for a Robot Keywords File.
    
    Added action to the "New" group in the project explorer.

---
+++ src/main/java/com/jivesoftware/robot/intellij/plugin/actions/NewRobotFileAction.java
@@ -0,0 +1,33 @@
+package com.jivesoftware.robot.intellij.plugin.actions;
+
+import com.intellij.ide.actions.CreateFileFromTemplateAction;
+import com.intellij.ide.actions.CreateFileFromTemplateDialog;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiDirectory;
+import com.jivesoftware.robot.intellij.plugin.icons.RobotIcons;
+
+import javax.swing.*;
+
+public class NewRobotFileAction extends CreateFileFromTemplateAction {
+  public NewRobotFileAction() {
+    super("NewRobotFile.robot", "Create a new robot test case file", RobotIcons.FILE);
+  }
+
+  public NewRobotFileAction(String text, String description, Icon icon) {
+    super(text, description, icon);
+  }
+
+  @Override
+  protected void buildDialog(Project project, PsiDirectory directory, CreateFileFromTemplateDialog.Builder builder) {
+    builder
+        .setTitle("New Robot File")
+        .addKind("Robot Test Case File", RobotIcons.FILE, "RobotTestCaseFile")
+        .addKind("Robot Keywords File", RobotIcons.FILE, "RobotKeywordsFile");
+
+  }
+
+  @Override
+  protected String getActionName(PsiDirectory directory, String newName, String templateName) {
+    return String.format("Create Robot file %s", newName);
+  }
+}
